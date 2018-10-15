b1c241c856db0fcf4e6c3b0acdb46202d98e905e
kevin907 <kevintrivedi99@gmail.com>
2018-10-15 19:56:49 +0530

    changed db connection for staging

--- knexfile.js
+++ knexfile.js
@@ -21,7 +21,7 @@ module.exports = {
   staging: {
     client: 'pg',
     connection: {
-      host: 'ec2-184-73-197-211.compute-1.amazonaws.com'
+      host: 'ec2-184-73-197-211.compute-1.amazonaws.com',
       database: 'da4lm1jl6mmb1i',
       user:     'mcdkhzcavrfeic',
       password: 'd7153584472d61f360c735b07132364818a21cf1f17f2c0dfd6086caea811398'
