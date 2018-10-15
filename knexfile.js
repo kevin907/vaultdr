1c4ea5052a34a461f274731e8a46c31ff37e2c28
kevin907 <kevintrivedi99@gmail.com>
2018-10-15 20:10:35 +0530

    changed db connection for staging

--- knexfile.js
+++ knexfile.js
@@ -23,6 +23,7 @@ module.exports = {
     connection: {
       host: 'ec2-184-73-197-211.compute-1.amazonaws.com',
       database: 'da4lm1jl6mmb1i',
+      port: '5432',
       user:     'mcdkhzcavrfeic',
       password: 'd7153584472d61f360c735b07132364818a21cf1f17f2c0dfd6086caea811398'
     },
